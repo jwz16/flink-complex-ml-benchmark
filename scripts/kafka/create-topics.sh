d6f517ed096cc3064dc5df3379f85969eac0e1cd
Sophie Zhang <jwz16@bu.edu>
2023-07-17 17:52:38 +0000

    fix typo

--- scripts/kafka/create-topics.sh
+++ scripts/kafka/create-topics.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ocker compose -f ../../docker/kafka/docker-compose.yml exec kafka kafka-topics.sh --create --topic complex-ml-input --bootstrap-server localhost:9092
+docker compose -f ../../docker/kafka/docker-compose.yml exec kafka kafka-topics.sh --create --topic complex-ml-input --bootstrap-server localhost:9092
